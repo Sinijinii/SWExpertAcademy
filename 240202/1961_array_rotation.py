d11bce7a456569f4b0ac17a114940674e580e909
sinijini <gmlwls2407@gmail.com>
2024-02-02 17:03:11 +0900

    Pass the array_rotation question and Add comment

--- 240202/1961_array_rotation.py
+++ 240202/1961_array_rotation.py
@@ -1,8 +1,10 @@
+# 회전할 함수 생성
 def rotation(arr):
     arr_ro = []
     for i in range(N):
         arr_3 = []
         for j in range(N):
+            # [N-j-1][i] = 하 -> 상 / 왼 -> 오로 접근
             arr_3.append(arr[N - j - 1][i])
         arr_ro.append(arr_3)
     return arr_ro
@@ -15,6 +17,7 @@ for tc in range(T):
     arr_180 = rotation(arr_90)
     arr_270 = rotation(arr_180)
     print("#{}".format(tc+1))
+    # 90, 180, 270의 0번~N번의 리스트의 원소를 순서대로 접근
     for i in range(N):
         for j in range(N):
             print(arr_90[i][j], end="")
