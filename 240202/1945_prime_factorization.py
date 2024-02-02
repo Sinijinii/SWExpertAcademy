8829dd385412657f269f673d8847ebb62f790dc4
sinijini <gmlwls2407@gmail.com>
2024-02-02 14:24:02 +0900

    1945_prime_factorization

---
+++ 240202/1945_prime_factorization.py
@@ -0,0 +1,13 @@
+T = int(input())
+for tc in range(T):
+    N = int(input())
+    dt = [2,3,5,7,11] # 소인수분해를 하기 위해 나눌 인자
+    result_list = []  # 나눈 갯수를 담기 위한 리스트
+    for i in dt:
+        counts = 0
+        while N % i == 0:   # 나머지가 없을때만 돌기
+            N = N/i
+            counts += 1
+        result_list.append(counts)
+    print("#{}".format(tc+1), *result_list)
+
