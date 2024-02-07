02691197f1db41e6f4b1517014db221100df64e1
sinijini <gmlwls2407@gmail.com>
2024-02-07 16:44:14 +0900

    Pass password [Use Recursion Function]

---
+++ 240207/1234_Password.py
@@ -0,0 +1,17 @@
+def repeated(data):
+    idx = "-"
+    for i in range(len(data)-1):
+        if data[i] == data[i+1]:
+            idx = i + 1
+            break
+    if idx == "-":
+        return data
+    else:
+        rep_data = data[0:idx-1] + data[idx+1:]
+        return repeated(rep_data)
+
+
+T = 10
+for tc in range(T):
+    N_data, str_data = map(str,input().split())
+    print(f'#{tc+1} {repeated(str_data)}')
