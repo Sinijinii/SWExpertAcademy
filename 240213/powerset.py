305078553952a0227d98decce5bb38a4cf9c713d
sinijini <gmlwls2407@gmail.com>
2024-02-13 15:23:46 +0900

    subset - Disable bit operator

---
+++ 240213/powerset.py
@@ -0,0 +1,20 @@
+def f (i,k):
+    if i == k :
+        for j in range(k):
+            if bit[j]:
+                print(arr[j],end=" ")
+        print()
+    else:
+        # for j in range(2):
+        #     bit[i] = j
+        #     f(i+1,k)
+        bit[i] = 1
+        f(i+1, k)
+        bit[i] = 0
+        f(i+1, k)
+
+
+N = 4
+arr = [1,2,3,4]
+bit = [0]*N
+f(0,N)
